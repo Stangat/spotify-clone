7105ff5d5db13d27b8bb3d7a7dd2ea2b3df1d13b
Stas Gatilov <stasgat12@gmail.com>
2023-02-02 21:39:08 +0300

    fix: change import name

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 // import ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './ts/App';
+import App from './ts/Application';
 
 const container = document.getElementById('app');
 const root = createRoot(container!);
