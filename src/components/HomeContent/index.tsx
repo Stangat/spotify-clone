9a07a2b708af4ded021e4c565b2471ae2ed81c89
Stas Gatilov <stasgat12@gmail.com>
2023-02-04 18:58:47 +0300

    fix: fix HomeContent

--- src/components/HomeContent/index.tsx
+++ src/components/HomeContent/index.tsx
@@ -5,35 +5,35 @@ import { CardItem } from '../CardItem';
 
 type HomeContentProps = {
   token: string;
-}; 
+};
 
-export const HomeContent: React.FC<HomeContentProps> = (props) => {
-  const [playlist,setPlaylists] = useState([])
-  
-  const playlists = async () => {
-   const response = await getPlaylist(props);
-   setPlaylists(response)
-   console.log(response)
-  }
+export const HomeContent: React.FC<HomeContentProps> = props => {
+  // const [playlist, setPlaylists] = useState([]);
 
-  useEffect(()=>{
-    playlists()
-  },[])
-  
+  // const playlists = async () => {
+  //   const response = await getPlaylist(props);
+  //   setPlaylists(response);
+  //   console.log(response);
+  // };
+
+  useEffect(() => {
+    // playlists();
+  }, []);
+
   return (
     <Content
       style={{
-        margin: '24px 16px 0',
+        // margin: '24px 16px 0',
         overflow: 'initial',
         backgroundColor: '#1e1e1e',
         color: 'white',
         fontWeight: 600,
-        display: 'flex',
-        flexWrap: 'wrap'
+        // display: 'flex',
+        flexWrap: 'wrap',
       }}
     >
-      <CardItem token={props.token}/>
-{/*       <CardItem />
+      <CardItem token={props.token} />
+      {/*       <CardItem />
       <CardItem />
       <CardItem />
       <CardItem />
