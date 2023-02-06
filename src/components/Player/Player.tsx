f9f2ab390dc5b7d6e237db78415b5ccb66b0be77
Stas Gatilov <stasgat12@gmail.com>
2023-02-06 11:16:23 +0300

    feat: add player block and volume block

--- src/components/Player/Player.tsx
+++ src/components/Player/Player.tsx
@@ -1,8 +1,15 @@
-import { HeartOutlined } from '@ant-design/icons';
-import { PicRightOutlined } from '@ant-design/icons';
+import { HeartOutlined, StepBackwardOutlined, PlayCircleOutlined, StepForwardOutlined } from '@ant-design/icons';
+import ReactSlider from 'react-slider';
+import ShuffleIcon from '@mui/icons-material/Shuffle';
+import RepeatIcon from '@mui/icons-material/Repeat';
+import PictureInPictureAltIcon from '@mui/icons-material/PictureInPictureAlt';
+import LyricsIcon from '@mui/icons-material/Lyrics';
+import QueueMusicIcon from '@mui/icons-material/QueueMusic';
+import { VolumeUp, VolumeOff, OpenInFull, PlayCircleFilled } from '@mui/icons-material';
 import { useEffect, useState } from 'react';
 import { getCurrentlyPlayingTrack } from '../../../api/api';
 import style from './player.module.less';
+import './sliders.css';
 
 type Props = {
   token: string;
@@ -46,9 +53,42 @@ const SongBlock = (props: Props) => {
         <p className={style.artistName}>{artistName}</p>
       </div>
       <div className={style.songInteractionContainer}>
-        <HeartOutlined style={{padding: '5px', fontSize: '18px'}} />
-        <PicRightOutlined style={{padding: '5px', fontSize: '18px'}} />
+        <HeartOutlined style={{ padding: '5px', fontSize: '18px' }} />
+        <PictureInPictureAltIcon style={{ fontSize: '20px', marginLeft: '10px' }} />
       </div>
+    </div>
+  );
+};
+
+const PlayerControls = () => {
+  return (
+    <div className={style.controlsContainer}>
+      <div className={style.buttonsContainer}>
+        <ShuffleIcon />
+        <StepBackwardOutlined style={{ fontSize: '1.5rem' }} />
+        <PlayCircleFilled style={{ fontSize: '2.5rem' }} />
+        <StepForwardOutlined style={{ fontSize: '1.5rem' }} />
+        <RepeatIcon />
+      </div>
+      <div className={style.sliderContainer}>
+        <span className={style.start}>00:00</span>
+        <ReactSlider className="slider" trackClassName="slider-track" thumbClassName="slider-thumb" />
+        <span className={style.duration}>00:00</span>
+      </div>
+    </div>
+  );
+};
+
+const VolumeBlock = () => {
+  return (
+    <div className={style.volumeBlockContainer}>
+      <LyricsIcon style={{ fontSize: '20px' }} />
+      <QueueMusicIcon style={{ fontSize: '25px' }}/>
+      <VolumeUp />
+      <div className={style.volumeSliderContainer}>
+        <ReactSlider className="slider" trackClassName="slider-track" thumbClassName="slider-thumb" />
+      </div>
+      <OpenInFull style={{ fontSize: '20px' }} />
     </div>
   );
 };
@@ -57,6 +97,8 @@ export const Player = (props: Props) => {
   return (
     <div className={style.playerContainer}>
       <SongBlock token={props.token} />
+      <PlayerControls />
+      <VolumeBlock />
     </div>
   );
 };
