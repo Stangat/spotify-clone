999d4866d87ccbebd9c1917c95c1db9801282ae6
Stas Gatilov <stasgat12@gmail.com>
2023-02-10 18:08:18 +0300

    fix: remove unused functions, fix getTrack

--- api/api.tsx
+++ api/api.tsx
@@ -61,84 +61,14 @@ export async function getPlaylist({ token }: Token) {
   return playlists;
 }
 
-export async function getCurrentlyPlayingTrack({ token }: Token) {
-  const res = await fetch('https://api.spotify.com/v1/me/player/currently-playing', {
+export async function getTrack(token: string, id: string) {
+  const res = await fetch(`https://api.spotify.com/v1/tracks/${id}?market=ES`, {
     method: 'GET',
     headers: {
       Authorization: `Bearer ${token}`,
       'Content-Type': 'application/json',
     },
   });
-  const { item } = await res.json();
-  return item;
-}
-
-export async function getPlaybackState({ token }: Token) {
-  const res = await fetch('https://api.spotify.com/v1/me/player', {
-    method: 'GET',
-    headers: {
-      Authorization: `Bearer ${token}`,
-      'Content-Type': 'application/json',
-    },
-  });
-  const result = await res.json();
-  return result;
-}
-
-export async function getAvailableDevices({ token }: Token) {
-  const res = await fetch('https://api.spotify.com/v1/me/player/devices', {
-    method: 'GET',
-    headers: {
-      Authorization: `Bearer ${token}`,
-      'Content-Type': 'application/json',
-    },
-  });
-  const result = await res.json();
-  return result;
-}
-
-export async function getTrack({ token }: Token) {
-  const res = await fetch('https://api.spotify.com/v1/tracks/11dFghVXANMlKmJXsNCbNl?market=ES', {
-    method: 'GET',
-    headers: {
-      Authorization: `Bearer ${token}`,
-      'Content-Type': 'application/json',
-    },
-  });
-  // const rs = res.arrayBuffer();
-  // return rs
-  // const reader = rs?.getReader();
   const track = await res.json();
-  console.log(track);
-
-  // if (reader) {
-  //   return reader.read().then(result => {
-  //     return result;
-  //   });
-  // }
-}
-
-export async function startPlayback({ token }: Token, id: string) {
-  const body = {
-    position_ms: 0,
-  };
-
-  await fetch(`https://api.spotify.com/v1/me/player/play?device_id=${id}`, {
-    method: 'PUT',
-    headers: {
-      Authorization: `Bearer ${token}`,
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(body),
-  });
-}
-
-export async function pausePlayback({ token }: Token, id: string) {
-  await fetch(`https://api.spotify.com/v1/me/player/pause?device_id=${id}`, {
-    method: 'PUT',
-    headers: {
-      Authorization: `Bearer ${token}`,
-      'Content-Type': 'application/json',
-    },
-  });
+  return track;
 }
