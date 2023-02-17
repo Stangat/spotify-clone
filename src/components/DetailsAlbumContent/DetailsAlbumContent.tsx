92c3b4a7dabe10a85d6edfbf8b562066fd1f4e5c
Stas Gatilov <stasgat12@gmail.com>
2023-02-17 15:42:53 +0300

    fix: change album content hearder view

--- src/components/DetailsAlbumContent/DetailsAlbumContent.tsx
+++ src/components/DetailsAlbumContent/DetailsAlbumContent.tsx
@@ -88,16 +88,15 @@ export const DetailsAlbumContent: React.FC<DetailsAlbumContentProps> = props => 
                 }}
               >
                 <div className={styles.boxBlur}>
-                  <img
-                    className={styles.imageAlbum}
-                    key={album.id}
-                    alt={album.label}
-                    src={album.images[1].url}
-                    style={{ boxShadow: '0px 0px 5px 0px black' }}
-                  />
+                  <img className={styles.imageAlbum} key={album.id} alt={album.label} src={album.images[1].url} />
                   <div className={styles.descriptionAlbumTracks}>
                     <p className={styles.typeAlbum}>{album.type}</p>
-                    <p className={styles.albumName}>{album.name}</p>
+                    {album.name.length > 17 ? (
+                      <p className={styles.albumNameMini}>{album.name}</p>
+                    ) : (
+                      <p className={styles.albumName}>{album.name}</p>
+                    )}
+
                     <div className={styles.descriptionBottom}>
                       <div className={styles.artistNameHeader}>
                         {album.artists.map(artist => {
