0b66002adf8c412b1f0a95378f06fb0544ee5d2d
MarinaZai <marintos34@gmail.com>
2023-02-17 20:51:06 +0300

    feat:start create settings page

--- src/pages/Settings/Settings.tsx
+++ src/pages/Settings/Settings.tsx
@@ -1,3 +1,56 @@
-export const Settings: React.FC = () => {
-  return <div style={{ color: 'red' }}>Settings Page</div>;
+import { Select } from 'antd';
+import { ProfileType } from '../../../interface/interface';
+import { DropdownProfile } from '../../components/Dropdown/DropDown';
+import style from './settings.module.less';
+type SettingsProps = {
+  token: string;
+  setToken: (token: string) => void;
+  profile: ProfileType | undefined;
+  setProfile: (profile: ProfileType) => void;
+};
+const onChange = (value: string) => {
+  console.log(`selected ${value}`);
+};
+
+const onSearch = (value: string) => {
+  console.log('search:', value);
+};
+export const Settings: React.FC<SettingsProps> = props => {
+  return (
+    <div className={style.wrapper}>
+      <DropdownProfile
+        setToken={props.setToken}
+        profile={props.profile}
+        setProfile={props.setProfile}
+        token={props.token}
+      />
+      <div className={style.wrapperChoose}>
+        <span className={style.titleBlock}>Settings</span>
+        <div className={style.blockChoose}>
+          <div className={style.selectBlock}>
+            <span className={style.selectTitle}>Language</span>
+            <span className={style.chooseTitle}>Choose language</span>
+          </div>
+          <Select
+            showSearch
+            placeholder="Select language"
+            optionFilterProp="children"
+            onChange={onChange}
+            onSearch={onSearch}
+            filterOption={(input, option) => (option?.label ?? '').toLowerCase().includes(input.toLowerCase())}
+            options={[
+              {
+                value: 'english',
+                label: 'English',
+              },
+              {
+                value: 'russian',
+                label: 'Russian',
+              },
+            ]}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
