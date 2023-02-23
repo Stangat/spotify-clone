6955388228781412af5c0548f6ecf63f30cc2ebc
Stas Gatilov <stasgat12@gmail.com>
2023-02-23 20:48:20 +0300

    fix: add check for avatar absence

--- src/components/DetailsProfilePage/DetailsProfilePage.tsx
+++ src/components/DetailsProfilePage/DetailsProfilePage.tsx
@@ -1,14 +1,15 @@
-import { Avatar} from 'antd';
-import {
-  ITrackTypes,
-  PlaylistsType,
-  ProfileType,
-  TopArtistsType,
-} from '../../../interface/interface';
+import { Avatar } from 'antd';
+import { ITrackTypes, PlaylistsType, ProfileType, TopArtistsType } from '../../../interface/interface';
 import styles from './detailsProfilePage.module.less';
 import { UserOutlined } from '@ant-design/icons';
-import { getFollowedArtists, getUserPlaylistsSpotifyApi, getUserTopArtistsSpotifyApi, getUserTopTracks, getUserTopTracksSpotifyApi } from '../../../api/api';
-import { useEffect, useState} from 'react';
+import {
+  getFollowedArtists,
+  getUserPlaylistsSpotifyApi,
+  getUserTopArtistsSpotifyApi,
+  getUserTopTracks,
+  getUserTopTracksSpotifyApi,
+} from '../../../api/api';
+import { useEffect, useState } from 'react';
 import { DropDownCopy } from '../DropDownCopy/DropDownCopy';
 import { useTranslation } from 'react-i18next';
 import { RowOfCards } from '../RowOfCards/RowOfCards';
@@ -48,7 +49,7 @@ export const DetailsProfilePage: React.FC<DetailsProfilePageProps> = props => {
   const [userPlaylists, setUserPlaylists] = useState<SpotifyApi.ListOfCurrentUsersPlaylistsResponse>();
   const { t } = useTranslation();
   const navigate = useNavigate();
-  
+
   const getTopTracksUserHandler = async () => {
     const response = await getUserTopTracks({ token: props.token, limit: 4 });
     const response2 = await getUserTopTracksSpotifyApi(props.token, 4);
@@ -59,18 +60,18 @@ export const DetailsProfilePage: React.FC<DetailsProfilePageProps> = props => {
   const getUserPlaylists = async () => {
     const response = await getUserPlaylistsSpotifyApi(props.token);
     setUserPlaylists(response);
-  }
+  };
 
   const getFollowedArtistsList = async () => {
     const response = await getFollowedArtists(props.token);
     setFollowedArtists(response);
-  }
+  };
 
   const getUserTopArtistList = async () => {
     const response = await getUserTopArtistsSpotifyApi(props.token);
-    response.items.sort((l, r) => r.popularity - l.popularity)
+    response.items.sort((l, r) => r.popularity - l.popularity);
     setTopArtists(response);
-  }
+  };
 
   useEffect(() => {
     getTopTracksUserHandler();
@@ -83,23 +84,36 @@ export const DetailsProfilePage: React.FC<DetailsProfilePageProps> = props => {
     <div className={styles.detailsProfileContainer}>
       <div className={styles.blockProfileDescription} key={props.profile?.id}>
         <div className={styles.imageContainer}>
-          <Avatar style={{height: '100%', width: '100%'}} icon={<UserOutlined />} src={props.profile?.images[0].url} />
+          <Avatar
+            style={{ height: '100%', width: '100%' }}
+            icon={<UserOutlined />}
+            src={props.profile?.images[0] ? props.profile?.images[0].url : ''}
+          />
         </div>
         <div className={styles.descriptionProfile}>
           <h2>{t('profile')}</h2>
           <h1 className={styles.userNameProfile}>{props.profile?.display_name}</h1>
           <div className={styles.descriptionItems}>
-            <p>{props.playlists?.total} {t('publicPlaylists')}</p>
-            <p>{followedArtists?.artists ? followedArtists?.artists.total : '0'} {t('following')}</p>
+            <p>
+              {props.playlists?.total} {t('publicPlaylists')}
+            </p>
+            <p>
+              {followedArtists?.artists ? followedArtists?.artists.total : '0'} {t('following')}
+            </p>
           </div>
         </div>
       </div>
       <div className={styles.bgUnderHat}></div>
       <div className={styles.bodyWrapper}>
-        <div className={styles.dropDown}><DropDownCopy /></div>
+        <div className={styles.dropDown}>
+          <DropDownCopy />
+        </div>
         <div className={styles.followingContainer}>
           <RowOfCards title={'Top artists this month'} subtitle={`${t('visible')}`}>
-            {topArtists?.items && topArtists?.items?.map((e, i) => i < 8 ? <CardArtist style={{boxShadow: 'none',}} key={e.id} artist={e}></CardArtist> : '')}
+            {topArtists?.items &&
+              topArtists?.items?.map((e, i) =>
+                i < 8 ? <CardArtist style={{ boxShadow: 'none' }} key={e.id} artist={e}></CardArtist> : ''
+              )}
           </RowOfCards>
         </div>
         <div className={styles.subblockContainer}>
@@ -108,13 +122,18 @@ export const DetailsProfilePage: React.FC<DetailsProfilePageProps> = props => {
               <h2>{t('topTrack')}</h2>
               <p>{t('visible')}</p>
             </div>
-            <p className={styles.showAll}onClick={() => { navigate(`/top_tracks`);}}>
-               {t('showAll')}
+            <p
+              className={styles.showAll}
+              onClick={() => {
+                navigate(`/top_tracks`);
+              }}
+            >
+              {t('showAll')}
               {/* пока пусть будет так, но нужyо писать роуты на все show all в этой странице */}
             </p>
           </div>
           <div className={styles.topTracksContainer}>
-              {/* {topTracksSpotifyApi?.items && topTracksSpotifyApi?.items.map((e)=> <TrackRow key={e.id} track={e}></TrackRow>)} */}
+            {/* {topTracksSpotifyApi?.items && topTracksSpotifyApi?.items.map((e)=> <TrackRow key={e.id} track={e}></TrackRow>)} */}
           </div>
           {/* <TopTracksBlock
             topTracks={props.topTracks}
@@ -136,30 +155,50 @@ export const DetailsProfilePage: React.FC<DetailsProfilePageProps> = props => {
             setAlbumTracks={props.setAlbumTracks}
             shuffle={props.shuffle}
             setShuffle={props.setShuffle}
-          />  */} 
+          />  */}
           {/* ЛИБО ТО ЛИБО ЭТО */}
         </div>
         <div className={styles.subblockContainer}>
           <div className={styles.blockHeader + ' ' + styles.withoutVisibility}>
             <h2>{'Public Playlists'}</h2>
-            <p className={styles.showAll} onClick={() => {/*смотри коммент выше*/}}> {t('showAll')}
+            <p
+              className={styles.showAll}
+              onClick={() => {
+                /*смотри коммент выше*/
+              }}
+            >
+              {' '}
+              {t('showAll')}
             </p>
           </div>
           <div>
             <RowOfCards>
-              {userPlaylists?.items && userPlaylists?.items.map((e, i) => (i < 8 ? <CardItem key={e.id} album={e} onClick={() => navigate(`/playlist/${e.id}`)}></CardItem> : ''))}
+              {userPlaylists?.items &&
+                userPlaylists?.items.map((e, i) =>
+                  i < 8 ? <CardItem key={e.id} album={e} onClick={() => navigate(`/playlist/${e.id}`)}></CardItem> : ''
+                )}
             </RowOfCards>
           </div>
         </div>
         <div className={styles.subblockContainer}>
           <div className={styles.blockHeader + ' ' + styles.withoutVisibility}>
             <h2>{'Following'}</h2>
-            <p className={styles.showAll} onClick={() => {/*смотри коммент выше*/}}> {t('showAll')}
+            <p
+              className={styles.showAll}
+              onClick={() => {
+                /*смотри коммент выше*/
+              }}
+            >
+              {' '}
+              {t('showAll')}
             </p>
           </div>
           <div>
             <RowOfCards>
-              {followedArtists?.artists.items && followedArtists.artists.items.map((e, i) => (i < 8 ? <CardArtist key={e.id} artist={e}></CardArtist> : ''))}
+              {followedArtists?.artists.items &&
+                followedArtists.artists.items.map((e, i) =>
+                  i < 8 ? <CardArtist key={e.id} artist={e}></CardArtist> : ''
+                )}
             </RowOfCards>
           </div>
         </div>
